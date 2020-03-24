9ebc0dcacfcfad347eeeb314dd94ffd167bb0d27
Cole Arendt <cole.arendt@outlook.com>
2020-03-24 12:35:22 +0000

    add a redirect endpoint

---
+++ plumber.R
@@ -0,0 +1,44 @@
+#
+# This is a Plumber API. You can run the API by clicking
+# the 'Run API' button above.
+#
+# Find out more about building APIs with Plumber here:
+#
+#    https://www.rplumber.io/
+#
+
+library(plumber)
+
+#* @apiTitle Plumber Example API
+
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+function(msg = "test") {
+    list(msg = paste0("The message is: '", msg, "'"))
+}
+
+#* Redirect to another location
+#* @param msg The message to echo
+#* @get /redirect
+function(res, msg = "test") {
+   #browser()
+   res$status <- 302
+   res$setHeader("Location", "/echo")
+}
+
+#* Plot a histogram
+#* @png
+#* @get /plot
+function() {
+    rand <- rnorm(100)
+    hist(rand)
+}
+
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
+#* @post /sum
+function(a, b) {
+    as.numeric(a) + as.numeric(b)
+}
